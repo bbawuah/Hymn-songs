c83c5681dca80d9b909aef3f8d576af7881990ea
Brian Bawuah <brianbawuah@icloud.com>
2019-12-05 11:50:59 +0100

    first commit

--- serve_html/app.js
+++ serve_html/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const hbs = require('hbs')
 const app = express();
 
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000;
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
